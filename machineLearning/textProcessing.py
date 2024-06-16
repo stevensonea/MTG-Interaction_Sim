c9ee10b64b580d2acecdf093809a4089e4eb9334
stevenea <stevenea@iu.edu>
2024-06-16 05:41:11 -0500

    text processing based on BERT model

--- machineLearning/textProcessing.py
+++ machineLearning/textProcessing.py
@@ -1,46 +1,20 @@
-# Author: Ethan Stevenson
-# Purpose: To learn how machine learning can read text following a tutorial found at https://towardsdatascience.com/machine-learning-text-processing-1d5a2d638958
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import pipeline
 
-"""
-There are 4 steps when it comes to Text Processing
-    1) Data Preprocessing
-        - Tokenization: converting sentences to words
-        - Removing unnecessary punctuation (tags)
-        - Removing stop words: words that do not have specific sematic (ex. "the", "is")
-        - Stemming: reducing words to their root by dropping unecessary characters. Doing this removes inflection
-        - Lemmatization: removes inflection by determining parts of speech
-"""
+# Load pre-trained model and tokenizer
+model_name = "dbmdz/bert-large-cased-finetuned-conll03-english"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForTokenClassification.from_pretrained(model_name)
 
-# The Natural Language ToolKit does most of the text preprocessing 
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
-from nltk.stem import WordNetLemmatizer
+# Create NER pipeline
+ner_pipeline = pipeline("ner", model=model, tokenizer=tokenizer)
 
-## Step 1: Data Processing ##
-#This function tokenizes the sentence
-tokens = word_tokenize("The quick brown fox jumps over the lazy dog")
-print("After tokenization\n", tokens)
+# Input text
+text = "Hugging Face Inc. is a company based in New York City. Its headquarters are in DUMBO, therefore very close to the Manhattan Bridge."
 
-#Now we will remove the stop words
-nltk.download('stopwords')
-stop_words = set(stopwords.words('english'))
-tokens = [w for w in tokens if not w in stop_words]
-print("After removing stop words\n", tokens)
+# Perform NER
+entities = ner_pipeline(text)
 
-#Now we will stem the words
-porter = PorterStemmer()
-stems = []
-for t in tokens:
-    stems.append(porter.stem(t))
-print("After stemming\n", stems)
-
-#Last thing for this step, we lemmatize the stems
-lemmatizer = WordNetLemmatizer()
-lemmatized = []
-for s in stems:
-    lemmatized.append(lemmatizer.lemmatize(s))
-print("After lemmatization\n", lemmatized)
-
-## Step 2: Feature Extraction ##
+# Display results
+for entity in entities:
+    print(f"Entity: {entity['word']}, Label: {entity['entity']}, Score: {entity['score']:.2f}")
