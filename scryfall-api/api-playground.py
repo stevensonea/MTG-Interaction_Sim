4595d4b50ad25a4860d593609a8a13daf2b99b77
stevenea <stevenea@iu.edu>
2024-06-16 06:13:54 -0500

    successfully pull json files from a card search

--- scryfall-api/api-playground.py
+++ scryfall-api/api-playground.py
@@ -1,28 +1,37 @@
 import requests
 import json
+import os
+from time import sleep
 
-def searchCard(card_name):
+def searchCard(cards):
     url = "https://api.scryfall.com/cards/search"
-    params = {
-        'q': card_name
-    }
-    response = requests.get(url, params=params)
 
-    # Check if the request was successful
-    if response.status_code == 200:
-        data = response.json()
+    for card in cards:
+        params = {
+            'q': card
+        }
+        response = requests.get(url, params=params)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            data = response.json()
+            
+            directory = "scryfall-api/json-files"
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+
+            filename = os.path.join(directory, card + ".json")
+
+            with open(filename, 'w') as file:
+                json.dump(data, file, indent=4)
+
+            print("Success")
         
-        filename = card_name + str(".json")
-        with open(filename, 'w') as file:
-            json.dump(data, file, indent=4)
-
-        print("Success")
-        return None
-    
-    else:
-        print(f"Error: {response.status_code}")
-        return None
+        else:
+            print(f"Error: {response.status_code}")
+        
+        sleep(.1)
     
 if __name__ == "__main__":
-    card_name = "Black Lotus"
-    card_data = searchCard(card_name)
+    cards = ["Black Lotus", "Ashling", "Sol Ring", "Sulfer"]
+    card_data = searchCard(cards)
